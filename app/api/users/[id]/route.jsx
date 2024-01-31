1a5c994cfc2714f63a05f9a5bd583dabe36a590e
Diego Ortiz <diego.ortiz.barbosa94@gmail.com>
2024-01-30 22:52:41 -0800

    starting doing it right, adding routes to login and create user for mini api

--- app/api/users/[id]/route.jsx
+++ app/api/users/[id]/route.jsx
@@ -1,8 +1,30 @@
 import {NextResponse} from"next/server"
-import Result from "postcss/lib/result"
+import { users } from "../../../util/db"
 
-export function GET(_, response){
-    const {id} = response.params
+export async function GET(_, response){
+    const {id} = await response.params
 
-    return NextResponse.json({result: id})
+    const user = users.filter(u => u.id ==id)
+
+    return NextResponse.json({user},{status: 200})
 }
+
+export async function POST(req, res){
+    let {name, email, pass} = await req.json()
+    const {id} = await res.params
+
+    const {name: uName, email: uemail, pass: uPass
+    } = users.find((u) => u.id == id);
+
+    if( uName === name && uemail ==email && pass === uPass ){
+        return NextResponse.json({result: "successful login"});
+    }
+    else if (!name || !email || !pass){
+        return NextResponse.json({result: "Please fill all the input data"});
+    }
+    else {
+        return NextResponse.json({result: "Invalid credentials"});
+    }
+    
+}
+
--- app/api/users/route.jsx
+++ app/api/users/route.jsx
@@ -1,6 +1,16 @@
-import { BaseNextResponse } from "next/dist/server/base-http";
-import {NextResponse} from"next/server"
 
-export function GET(params){
-    return NextResponse.json({result: 'hello'}, {status: 200})
+import {NextResponse} from"next/server"
+import { users } from "../../util/db"
+
+export function GET(){
+    const data = users
+    return NextResponse.json({data}, {status: 200})
+}
+
+export async function POST(req, resp){
+    let {id, name, email, pass} = await req.json()
+
+    if (!id || !name || !email || !pass){
+        return NextResponse.json({result: "field missing"}, {status: 400});
+    }
 }
